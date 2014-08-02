2b6bba46202b3ea692e21bc63d3d2f2725fcd430
Jesse Rosato <jesse.rosato@gmail.com>
2014-08-01 19:07:21 -0700

    Major changes. Now using anguler-leaflet-directive, including clustering. Added popups.

---
+++ public/javascripts/app.js
@@ -0,0 +1,2 @@
+var wicItApp = angular.module('wicItApp', ['notifications', 'geolocation', 'leaflet-directive']);
+wicItApp.controller('mapCtrl', mapCtrl);
---
+++ public/javascripts/controllers/map.js
@@ -0,0 +1,139 @@
+/** MAP CONTROLLER */
+var mapCtrl = function ($scope, $http, $timeout, leafletEvents, NotificationService, GeolocationService) {
+
+  var defaultZoom = 13;
+  var maxZoom = 18;
+  var pinIcon = iconFactory('image/pin.png', 'image/pin_shadow.png', 30, 30);
+  var markerIcon = iconFactory('image/marker.png', 'image/marker_shadow.png', 30, 30);
+  var tileUrl = "https://{s}.tiles.mapbox.com/v3/jesserosato.j200j557/{z}/{x}/{y}.png";
+  var locationsBaseUrl = 'http://health.data.ca.gov/resource/i7wi-ei4m.json';
+  var locationsAppToken = 'S0kfDwCy0pFWq18dpMK7JADbT';
+  var mapUpdating = false;
+
+  $scope.mapLoading = true;
+
+  initMap();
+
+  GeolocationService.getPosition().then(setUserLocation, geolocationError);
+
+  $scope.$on('leafletDirectiveMap.moveend', function(event){
+    if ( ! mapUpdating) {
+      updateNearbyLocations(event).then(function() { mapUpdating = false });
+    }
+  });
+
+  function initMap()
+  {
+    angular.extend($scope, {
+      defaults: {
+        tileLayer: tileUrl,
+        maxZoom: maxZoom,
+        zoomControlPosition: 'topright',
+        tileLayerOptions: {
+          detectRetina: true,
+          reuseTiles: true
+        }
+      },
+      // Use Sacramento as default center
+      center: {
+        lat: 38.5556,
+        lng: -121.4689,
+        zoom: defaultZoom
+      },
+      markers: {
+        user: {
+          lat: 38.5556,
+          lng: -121.4689,
+          focus: true,
+          icon: pinIcon
+        }
+      }
+    });
+  }
+
+  function setUserLocation(position)
+  {
+    $scope.mapLoading = false;
+    var lat = position.coords.latitude;
+    var lng = position.coords.longitude;
+    $scope.center = {
+      lat: lat,
+      lng: lng,
+      zoom: defaultZoom
+    };
+    $scope.markers.user.lat = lat;
+    $scope.markers.user.lng = lng;
+  }
+
+  function geolocationError(geolocationError) {
+    var message = geolocationError.code == geolocationError.PERMISSION_DENIED ? "Dang, geolocation is disabled." : "Dang, we can't get your location.";
+    NotificationService.addNotificiation({
+      message: message,
+      status: NotificationService.STATUSES.ERROR
+    });
+  }
+
+  /**
+   * Update the map with nearby locations
+   * TODO: Use real data.
+   * TODO: search in map bounding box
+   */
+  function updateNearbyLocations(event) {
+    mapUpdating = true;
+    var bounds = event.bounds;
+    // var nw = bounds.getNorthWest();
+    // var se = bounds.getSouthEast();
+    var locationsUrl = '/locations';
+    // var locationsUrl = locationsBaseUrl;
+    // locationsUrl += '&$where=within_box(location, ' + nw.lat + ', ' + nw.long + ', ' + se.lat + ', ' + se.long + ')';
+    return $http.get(locationsUrl).success(displayLocations).error(updateNearbyLocationsError);
+
+    function displayLocations(data) {
+      data.forEach(function (vendor) {
+        var lat = parseFloat(vendor.location.latitude);
+        var lng = parseFloat(vendor.location.longitude);
+        var name = vendor.vendor;
+        var city = vendor.city;
+        var zip = vendor.zip_code;
+        var address = vendor.address + ' ' + vendor.second_address + ', ' + city + ' ' + zip;
+        var message ='<h3>' + name + '</h3><p>' + address + '</p><p></p><a href="https://maps.google.com?saddr=Current+Location&daddr=' + address + '" target="_blank">Directions</a></p>';
+        $scope.markers[keyify(name)] = {
+          group: vendor.county,
+          lat: lat,
+          lng: lng,
+          focus: false,
+          message: message,
+          icon: markerIcon
+        }
+      });
+      return $timeout(function() {}, 1500);
+    }
+
+    function updateNearbyLocationsError(data, status, headers, config) {
+      NotificationService.addNotificiation({
+        message: "Unable to load nearby locations.",
+        status: NotificationService.STATUSES.ERROR
+      })
+    }
+
+  }
+
+  function keyify(orig)
+  {
+    return orig.replace(/\W/g, '');
+  }
+
+  function iconFactory(iconUrl, shadowUrl, iconHeight, iconWidth)
+  {
+    return {
+      iconUrl: iconUrl,
+      shadowUrl: shadowUrl,
+      iconSize: [iconHeight, iconWidth], // size of the icon
+      shadowSize: [iconHeight, iconWidth], // size of the shadow
+      iconAnchor: [0, iconWidth/2], // point of the icon which will correspond to marker's location
+      shadowAnchor: [0, iconWidth/2],  // the same for the shadow
+      popupAnchor: [iconWidth/2, 0] // point from which the popup should open relative to the iconAnchor
+    };
+  };
+
+};
