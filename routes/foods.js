cc007118c61ad0fa6f9e67a6cf9bd345d46ec0ae
Jesse Rosato <jesse.rosato@gmail.com>
2014-09-22 23:22:33 -0700

    Foods route now serving at /approved-foods?q=query_parameter.

--- routes/foods.js
+++ routes/foods.js
@@ -2,15 +2,43 @@ var fs = require('fs');
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET foods by search parameter. */
 router.get('/', function(req, res) {
-  fs.readFile('sample_data/foods.json', null, function(err, data) {
+  // Get the query parameter 'q'
+  var query = req.query.q;
+  if ( ! query) {
+    res.json([]);
+  }
+  // Save a lowercase version of the query.
+  var lcQuery = query.toLowerCase();
+  // Read load foods JSON from file.
+  fs.readFile('sample_data/foods.json', null, function (err, data) {
     if (err) {
       res.status(500).send("Error loading locations.");
     } else {
-      res.json(data);
+      var foods = JSON.parse(data);
+      // Filter by search term.
+      var filteredFoods = foods.filter(filterSearchResult);
+      res.json(filteredFoods);
     }
   });
+
+  /**
+   * Super simple search: "Does food object contain query string in any of its attributes?"
+   * TODO: Break query up by word.
+   * @param food
+   * @returns {boolean}
+   */
+  function filterSearchResult(food)
+  {
+    for(value in food) {
+      if (food[value].toLowerCase().match(lcQuery)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
 });
 
 module.exports = router;
