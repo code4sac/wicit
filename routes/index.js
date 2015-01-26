ac496875799d5e5f8b17f9333b82459269de912d
Don McCurdy <don.r.mccurdy@gmail.com>
2015-01-25 21:09:31 -0800

    Adding a meta description to layout file

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,10 @@ dotenv.load();
 router.get('/', function(req, res) {
   res.render('index', {
     title: 'WICit: Where to shop with WIC.',
+    description: 'Where and how to get and use WIC: '
+      + 'Find locations that accept WIC in California, '
+      + 'using data from the California Department of '
+      + 'Public Health.',
     mapboxId: process.env.MAPBOX_ID,
     mapboxToken: process.env.MAPBOX_TOKEN,
     apiToken: process.env.API_TOKEN
