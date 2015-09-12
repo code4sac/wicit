f06ed73ddc28aa6de320dbc3d601be4c5e608c69
Mikyla DeWitt <mikyla.dewitt@gmail.com>
2015-09-11 22:19:21 -0700

    Testing #5  Add Karma Test Framework

--- Gruntfile.js
+++ Gruntfile.js
@@ -31,6 +31,18 @@ module.exports = function(grunt) {
           spawn: false // for grunt-contrib-watch v0.5.0+, "nospawn: true" for lower versions. Without this option specified express won't be reloaded
         }
       }
+    },
+    karma: {
+      unit: {
+        configFile: 'karma.conf.js',
+        singleRun: true
+      },
+      // The dev target can be run during development to have Karma continually running tests
+      // while making changes to code/test
+      dev: {
+        configFile: 'karma.conf.js',
+        singleRun: false
+      }
     }
   });
 
@@ -38,6 +50,8 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-express-server');
   grunt.loadNpmTasks('grunt-contrib-sass');
-  grunt.registerTask('build-dev', ['npm-install']);
+  grunt.loadNpmTasks('grunt-karma');
+
+  grunt.registerTask('build-dev', ['npm-install', 'karma:unit']);
   grunt.registerTask('dev', ['express:dev', 'watch']);
 }
