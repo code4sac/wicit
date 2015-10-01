9838dec1d5dea5377ee030e9ba76dd3d7a25d6fa
Jesse Rosato <jrosato@riskalyze.com>
2015-09-30 19:30:05 -0700

    Finish removing grunt-express

--- Gruntfile.js
+++ Gruntfile.js
@@ -70,7 +70,6 @@ module.exports = function (grunt) {
     });
 
     grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-express-server');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-ng-annotate');
     grunt.loadNpmTasks('grunt-sass');
